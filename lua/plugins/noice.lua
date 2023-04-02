4ffe17661478a607acdb1deb0b0f8cbb99ce3876
Andre Prolo <proloandre@hotmail.com>
2023-04-02 14:46:51 -0300

    initial commit

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,12 @@
+return {
+  { "folke/noice.nvim", enabled = false },
+}
+-- return {
+--   "folke/noice.nvim",
+--   messages = function(_, messages)
+--     messages.enabled = false
+--   end,
+--   popupmenu = function(_, popupmenu)
+--     popupmenu.enabled = false
+--   end,
+-- }
